3b28ce3fdb59bf524704deb625e83966772dc15e
Philipp Grosswiler <philipp.grosswiler@gmail.com>
2014-07-14 16:54:52 +0700

    Cleaned up code.

--- www/todataurl.js
+++ www/todataurl.js
@@ -25,33 +25,27 @@ var ToDataURL = {
 
     // Pass width and height of the bitmap data and get DataURL of the image.
     getImageData: function (data, width, height, type, quality) {
-console.log ('ToDataURL::getImageData');
         if (typeof quality === 'number') {
             quality *= 100;
         } else {
             quality = 100;
         }
-console.log ('ToDataURL::getImageData quality = ' + quality);
 
-console.log ('ToDataURL::getImageData = ' + data);
         exec(null, null, "ToDataURL", "getImageData", [data, width, height, type, quality]);
     },
 
     initialize: function () {
-console.log ('ToDataURL::initialize');
         var toDataURL = function (type, quality) {
-console.log ('ToDataURL::toDataURL = ' + type + ", " + quality);
-            // var imageData = Array.prototype.slice.call (this.getContext ("2d").getImageData (0, 0, this.width, this.height).data);
-
             return ToDataURL.getImageData (
                 btoa (
-                // Array.prototype.slice.call (
-                    this.getContext ("2d").getImageData (
-                        0,
-                        0,
-                        this.width,
-                        this.height
-                    ).data
+                    Array.prototype.slice.call (
+                        this.getContext ("2d").getImageData (
+                            0,
+                            0,
+                            this.width,
+                            this.height
+                        ).data
+                    )
                 ),
 
                 this.width,
@@ -79,12 +73,10 @@ console.log ('ToDataURL::_toDataURL = ' + result);
 
             if (result === "data:,") {
 */
-console.log ('ToDataURL CUSTOM FUNCTION');
                 HTMLCanvasElement.prototype.toDataURL = toDataURL;
                 return this.toDataURL.apply (this, arguments);
 /*
             } else {
-console.log ('ToDataURL NATIVE FUNCTION');
                 HTMLCanvasElement.prototype.toDataURL = _toDataURL;
                 return result;
             }
